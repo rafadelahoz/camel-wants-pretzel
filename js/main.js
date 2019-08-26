c48fe30cd81f7b6f4bcc16ab9a45fdf8f3c85c4c
Rafa de la Hoz <rhss@gmv.com>
2019-08-26 12:37:52 +0200

    Added basic entity management, rendering, testing fire

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 
 const charsetA = {
     "camel": "🐫",
-    "tree": "🌳"
+    "tree": "🌳",
+    "fire": "🔥",
+    "seed": "🌰"
 }
 
 const charsetB = {
     "camel": "C",
-    "tree": "T"
+    "tree": "T",
+    "fire": "F",
+    "seed": "S"
 }
 
-let charset = charsetB;
+let charset = charsetA;
 
 const cgame = {
     grid: [],
@@ -21,6 +25,15 @@ const cgame = {
         y: 4,
         char: charset.camel
     },
+
+    entities: [
+        {
+            "type": "fire",
+            x: 6, y: 6
+        }
+    ],
+
+    entitiesBuffer: [],
 
     init: function init() {
 
@@ -37,7 +50,7 @@ const cgame = {
                 if (col == 0 || col == this.cols-1 || row == 0 || row == this.rows-1)
                     cgame.grid[row].push(charset.tree);
                 else
-                    if (Math.random() > 0.5)
+                    if (Math.random() > 0.2)
                         this.grid[row].push("");
                     else
                         this.grid[row].push(charset.tree)
@@ -51,11 +64,13 @@ const cgame = {
         let screen = $("#screen");
         screen.html('');
 
+        let computedGrid = this.computeGrid();
+
         let innerText = "<table>";
         for (let row = 0; row < this.rows; row++) {
             innerText += "<tr>"
             for (let col = 0; col < this.cols; col++) {
-                innerText += this.renderCell(col, row);
+                innerText += "<td>" + computedGrid[row][col] + "</td>";
             }
             innerText += "</tr>";
         }
@@ -64,17 +79,70 @@ const cgame = {
         screen.append(innerText);
     },
 
-    renderCell: function renderCell(col, row) {
-        let cellText = "";
+    computeGrid: function computeGrid() {
+        var cgrid = [];
 
-        // First, camel
-        if (cgame.camel.x == col && cgame.camel.y == row)
-            cellText += "<td>" + this.camel.char + "</td>";
-        else
-        // Finally, the grid
-            cellText += "<td>" + this.grid[row][col] + "</td>";
+        // Put source grid
+        for (let row = 0; row < this.rows; row++) {
+            cgrid.push([]);
+            for (let col = 0; col < this.cols; col++) {
+                cgrid[row].push(this.grid[row][col]);
+            }
+        }
 
-        return cellText;
+        // Put entities
+        for (entity of this.entities) {
+            cgrid[entity.y][entity.x] = charset[entity.type];
+        }
+
+        // Put camel
+        cgrid[this.camel.y][this.camel.x] = this.camel.char;
+
+        return cgrid;
+    },
+
+    updateEntities: function updateEntities() {
+        // Update entities
+        for (entity of this.entities) {
+            this.updateEntity(entity);
+        }
+
+        // Clear buffer
+        for (entity of this.entitiesBuffer) {
+            this.entities.push(entity);
+        }
+
+        entitiesBuffer = [];
+    },
+
+    updateEntity: function updateEntity(entity) {
+        switch (entity.type) {
+            case "fire":
+                // Spread to trees
+                for (pos of this.buildNeighbours(entity)) {
+                    if (this.getGrid(pos.x, pos.y) == charset["tree"]) {
+                        this.setGrid(pos.x, pos.y, "x");
+                        this.entitiesBuffer.push({type: "fire", x: pos.x, y: pos.y});
+                    }
+                }
+                break;
+        }
+    },
+
+    setGrid: function setGrid(x, y, value) {
+        if (x >= 0 && x < this.cols && y >= 0 && y < this.rows)
+            this.grid[y][x] = value;
+    },
+
+    getGrid: function getGrid(x, y) {
+        if (x >= 0 && x < this.cols && y >= 0 && y < this.rows)
+            return this.grid[y][x];
+        return null;
+    },
+
+    buildNeighbours: function buildNeighbours(pos) {
+        return [{x: pos.x-1, y: pos.y}, {x: pos.x+1, y: pos.y},
+                {x: pos.x, y: pos.y-1}, {x: pos.x, y: pos.y+1}];
     },
 
     moveCamel: function moveCamel(dir) {
@@ -109,6 +177,8 @@ const cgame = {
             cgame.camel.x = nx;
             cgame.camel.y = ny;
         }
+
+        this.updateEntities();
 
         cgame.render();
     },
